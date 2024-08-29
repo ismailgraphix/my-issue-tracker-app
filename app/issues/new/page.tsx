bac767d4b7822451379f06ea2207fd5e082f29fa
ismailgraphix <ismailgrapix@gmail.com>
2024-08-29 18:26:18 +0100

    adding spinner to the submit button

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,10 +9,13 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/createIssueSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
+    const [isSubmitting, setSubmitting] = useState(false);
     const router = useRouter();
     const { register, control, handleSubmit, formState:{ errors }} = useForm<IssueForm>({
         resolver: zodResolver(createIssueSchema)
@@ -26,9 +29,12 @@ const NewIssuePage = () => {
     <form className=' space-y-3'
      onSubmit={handleSubmit(async (data) => {
         try {
+            setSubmitting(true);
             await axios.post('/api/issues', data);
      router.push('/issues');
+
         } catch (error) {
+            setSubmitting(false)
             setError('An unexpected error occured.')
         }
         
@@ -36,13 +42,15 @@ const NewIssuePage = () => {
       <TextField.Root>
         <TextField.Input placeholder='Title' {...register('title')} />
       </TextField.Root>
-      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+      <ErrorMessage>
+      {errors.title?.message}
+      </ErrorMessage>
       <Controller 
       name="description"
       control={control}
       render={({field}) => <SimpleMDE placeholder='Description' {...field} /> } />
-      {errors.description && <Text color="red" as="p">{errors.description.message} </Text>}
-      <Button   >Submit new Issue</Button>
+       <ErrorMessage>{errors.description?.message}</ErrorMessage>
+      <Button disabled={isSubmitting}  >Submit new Issue {isSubmitting &&  <Spinner/>} </Button>
     </form>
     </div>
   );
