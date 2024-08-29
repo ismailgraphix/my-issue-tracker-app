4abccfc420d87e0ebf82dad3995cd6f14c4f0147
ismailgraphix <ismailgrapix@gmail.com>
2024-08-29 17:23:26 +0100

    adding react mde form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,16 @@
 "use client";
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
 
 const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title'>
-        <TextField.Slot  />
+      <TextField.Root >
+        <TextField.Input placeholder='Title' />
       </TextField.Root>
-      <TextArea placeholder='Description' />
+      <SimpleMDE placeholder='Description' />
       <Button className='cursor-pointer'  >Submit new Issue</Button>
     </div>
   )
