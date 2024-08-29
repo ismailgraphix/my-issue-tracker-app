0e6d2ff5a5edb06ba374636beb7c14b7d341eb30
ismailgraphix <ismailgrapix@gmail.com>
2024-08-29 16:18:18 +0100

    database connection and issue model creation

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      issue
+    </div>
+  )
+}
+
+export default IssuesPage
---
+++ prisma/migrations/20240829145935_/migration.sql
@@ -0,0 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
+
+-- CreateTable
+CREATE TABLE "Issue" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Issue_pkey" PRIMARY KEY ("id")
+);
